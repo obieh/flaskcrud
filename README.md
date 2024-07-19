7fcdb32dbf7d4de4f864f94b9e2a5cc0d9d754fb
obieh <o_amadic@yahoo.com>
2024-07-19 18:57:58 +0100

    final work

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flaskcrud
+# simple CRUD Operation with python flask, html and css
+# All credit goes to Andrew Dass
--- app.py
+++ app.py
@@ -22,6 +22,16 @@ def crud_page():
 def post_page():
     return render_template("/post.html")
 
+# PUT
+@app.route("/put_elements")
+def add_put_elements():
+    dictionary = {}
+    file = open("dictionary_file.pkl", 'wb')
+    pickle.dump(dictionary, file)
+    file.close()
+    return render_template("/put.html")
+
+# Update Dictionary 
 @app.route("/success", methods=['POST'])
 def success():
     file = open("dictionary_file.pkl", 'rb')
@@ -39,6 +49,46 @@ def success():
 
     return render_template("/success.html", html_data_1=html_data_1, html_data_2=html_data_2)
 
+# GET
+@app.route("/get_page")
+def the_get_page():
+    file = open('dictionary_file.pkl', 'rb')
+    retrieve_dictionary = pickle.load(file)
+    file.close()
+    return render_template("/get.html", retrieve_dictionary=retrieve_dictionary)
+
+# DELETE
+@app.route("/delete_page")
+def the_delete_page():
+    file = open('dictionary_file.pkl', 'rb')
+    retrieve_dictionary = pickle.load(file)
+    file.close()
+    return render_template("/delete.html", retrieve_dictionary=retrieve_dictionary)
+
+@app.route("/delete_element", methods=['POST'])
+def delete_element():
+    file = open('dictionary_file.pkl', 'rb')
+    dictionary = pickle.load(file)
+    file.close()
+    key = request.form["entry1"]    
+
+    if key in dictionary.keys():
+        del dictionary[key]
+        file = open("dictionary_file.pkl", 'wb')
+        pickle.dump(dictionary, file)
+        file.close()
+        time.sleep(1)
+        return render_template("/deleteSuccess.html")
+    else:
+        return render_template("/error.html")
+    
+
+def clear_dictionary():
+    os.remove("dictionary_file.pkl")
+    dictionary = {}
+    file = open("dictionary_file.pkl", 'wb')
+    pickle.dump(dictionary, file)
+    file.close()
 
 if __name__== '__main__':
     execute_clear_dictionary.add_job(id = 'Scheduled Task', func=clear_dictionary, trigger="interval", seconds=300)
